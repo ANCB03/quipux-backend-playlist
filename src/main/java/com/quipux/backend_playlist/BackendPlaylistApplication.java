cd995720ed7246e81d09b959b14e3cdc714bce54
ANCB03 <ancabe1403@gmail.com>
2025-07-26 21:53:47 -0500

    feat: base project

---
+++ src/main/java/com/quipux/backend_playlist/BackendPlaylistApplication.java
@@ -0,0 +1,13 @@
+package com.quipux.backend_playlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPlaylistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendPlaylistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quipux/backend_playlist/BackendPlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quipux.backend_playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPlaylistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
