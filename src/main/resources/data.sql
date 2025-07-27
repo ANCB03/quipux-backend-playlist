2d64fb6cbb799a4924abb0cecd690dbc28fa2b05
ANCB03 <ancabe1403@gmail.com>
2025-07-26 22:27:45 -0500

    added data.sql with default users

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, username, email, password, roles) VALUES
+(1, 'admin', 'admin@gmail.com', '$2a$10$uG5kOPN/v1Nv1osR8lB9huNf8tG9O6jk5E/qAH1kjeg8nrCl2/P3S', 'ROLE_ADMIN'),
+(2, 'user', 'user@gmail.com','$2a$10$D6xTUnr1hC56sfVPx3JZsuioUYeMa8YHtuRJ4PT4ykp7J7FC1yW1W', 'ROLE_USER');
